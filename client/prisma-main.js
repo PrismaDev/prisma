9f36f540be0842163478819c515cbbe9cddaf2fb
Luiza Silva <luiza@royalsflush.net>
2012-11-16 16:12:54 -0200

    made table-like layout for main, auto adjusting top and bottom with jquery

--- client/prisma-main.js
+++ client/prisma-main.js
@@ -1,8 +1,26 @@
 var MainView = Backbone.View.extend({
 	el: 'body',
+
+	adjustSizes: function() {
+		var h = $(window).height();
+		var w = $(window).width();
+
+		var headerH = $("#main-header-div").height();
+		var footerH = $("#main-footer-div").height();
+
+		var middleH = h-headerH-footerH;
+		
+		$("#main-sidebar-div").css('top', parseFloat(headerH)+'px');
+		$("#main-timetable-div").css('top', parseFloat(headerH)+'px');
+	
+		$("#main-sidebar-div").css('bottom', parseFloat(footerH)+'px');
+		$("#main-timetable-div").css('bottom', parseFloat(footerH)+'px');	
+	},
+
 	render: function() {
 		var template = _.template($("#main-template").html());
 		this.$el.html(template);
+		this.adjustSizes();
 	}
 });
 
