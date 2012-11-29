e95244a79cfbbec4a22d6fe5a928dc97d25ddb0b
Julio Ribeiro <wwwjuliords@gmail.com>
2012-11-29 08:44:13 -0200

    doctrine isnt necessary anymore

--- Application/Framework/Main.php
+++ Application/Framework/Main.php
@@ -3,17 +3,12 @@
 namespace Framework;
 
 use Framework\Router;
-use Doctrine\Bootstrap as Doctrine;
 
 Class Main
 {
 	public static function init()
 	{
-		Doctrine::createConnection(include 'Config/db.php', array());
-
 		Router::init(include 'Config/router.php', $_SERVER['REQUEST_URI']);
-
-		Doctrine::closeConnection();
 	}
 }
 
