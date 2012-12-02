2e1886d866c49e059d4acc048e02f316b4175934
Luiza Silva <luiza@royalsflush.net>
2012-12-01 22:01:50 -0200

    Implemented table scroll change, but it only works when resizing the table

--- Public/js-dev/view/faltacursarView.js
+++ Public/js-dev/view/faltacursarView.js
@@ -7,7 +7,10 @@ var FaltacursarView = Backbone.View.extend({
 	classesDiv: '',
 	subjectTable: '',
 	subjectDatatable: '',
-	
+	subjectTableFilter: '',
+	subjectTableHeader: '',	
+	subjectTableBody: '',	
+
 	initialize: function() {
 		this.template = _.template($('#faltacursar-template').html());
 	},
@@ -20,6 +23,12 @@ var FaltacursarView = Backbone.View.extend({
 		this.subjectTableWrapper = $('#faltacursar-subject-table_wrapper');
 		this.classesDiv = $('#faltacursar-classes-div');
 		this.subjectTable = $('#faltacursar-subject-table');
+		this.subjectTableFilter = $('#faltacursar-subject-table_wrapper\
+					 .dataTables_filter');
+		this.subjectTableHeader = $('#faltacursar-subject-table_wrapper\
+					 .dataTables_scrollHead');
+		this.subjectTableBody = $('#faltacursar-subject-table_wrapper\
+					 .dataTables_scrollBody');
 	},
 
 	clickOnRow: function(e) {
@@ -30,7 +39,7 @@ var FaltacursarView = Backbone.View.extend({
         		
 			$(this.subjectTableWrapper).addClass('whole')
 						.removeClass('half');
-		
+			this.calculateTableScroll();		
 			$(this.classesDiv).addClass('hidden');
 		}
 		else {
@@ -40,7 +49,8 @@ var FaltacursarView = Backbone.View.extend({
 			
 			$(this.subjectTableWrapper).addClass('half')
 						.removeClass('whole');
-		
+			this.calculateTableScroll();		
+
         		$(this.classesDiv).removeClass('hidden');
 			$(this.classesDiv).addClass('almostHalf');	
 			faltacursarClasseslistView.resizeH();			
@@ -48,7 +58,13 @@ var FaltacursarView = Backbone.View.extend({
 	},
 
 	calculateTableScroll: function() {
-		
+		var h = $(this.subjectTableWrapper).height();				
+		var headerH = $(this.subjectTableFilter).outerHeight(true)+
+			$(this.subjectTableHeader).outerHeight(true);
+
+		var notHeight = $(this.subjectTableBody).outerHeight(true)-
+			$(this.subjectTableBody).height();
+		$(this.subjectTableBody).height(h-headerH-notHeight);
 	},
 
 	resizeW: function() {
@@ -56,7 +72,9 @@ var FaltacursarView = Backbone.View.extend({
 		this.calculateTableScroll();
 		faltacursarClasseslistView.resizeW();
 	},
-	resizeH: function() {},
+	resizeH: function() {
+		this.calculateTableScroll();
+	},
 
 	initJS: function() {
 		this.subjectDatatable = $('#faltacursar-subject-table').dataTable({
