29ddb5c18baadfae2d95c3e7151274ec37b630ad
Luiza Silva <luiza@royalsflush.net>
2012-12-05 14:41:35 -0200

    Almost done with login page

--- Public/js/prisma-login.min.js
+++ Public/js/prisma-login.min.js
@@ -2,7 +2,20 @@ var LoginStringsModel = Backbone.Model.extend({
 	defaults: {
 		'matriculaLabel': 'Matrícula',
 		'passwordLabel': 'Senha',
-		'submitButtonLabel': 'Login'
+		'submitButtonLabel': 'Login',
+		'loginHelpText': 'Para logar, use sua matrícula com sem o\
+			dígito verificador e a mesma senha que você utiliza\
+			para o PUC Online',
+		'forgotPasswordText': 'Se você esqueceu sua senha, <a href=\
+			"http://www.puc-rio.br/ensinopesq/academicas/">Clique aqui\
+			</a>para acessar o PUC Online e recuperá-la',
+		'infoText': 'O PrISMA, Programa de Instrução à Solicitação da\
+			Matrícula Acadêmica, é um sistema desenvolvido por alunos\
+			em parceria com o professor Sérgio Litschitz que tem como\
+			objetivo facilitar o preenchimento da primeira fase da\
+			matrícula',
+		'openLoginFormButton': 'Abre login',
+		'tutorialButton': 'Tutorial - em breve!'
 	}
 });
 
