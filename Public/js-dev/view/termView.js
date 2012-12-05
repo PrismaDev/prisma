b7a15b7aa46818fb72b7959737eb9d3755a659f7
Luiza Silva <luiza@royalsflush.net>
2012-12-05 18:09:03 -0200

    Finished term page

--- Public/js-dev/view/termView.js
+++ Public/js-dev/view/termView.js
@@ -1,12 +1,40 @@
 var TermView = Backbone.View.extend({
 	el: 'body',
+	layoutTemplate: '',
+	termTemplate: '',
+
+	events: {
+		'click input[type="submit"]': 'checkAcceptance',
+		'change input[type="checkbox"]': 'toggleContinueButton'
+	},
+
+	checkAcceptance: function() {
+		if ($('input[type="checkbox"]').is(':checked'))
+			return true;
+		return false;
+	},
+
+	toggleContinueButton: function() {
+		if ($('input[type="checkbox"]').is(':checked'))
+			$('input[type="submit"]').removeClass('disabled');
+		else
+			$('input[type="submit"]').addClass('disabled');	
+	},
+
+	initialize: function() {
+		this.layoutTemplate = _.template($("#layout-template").html());
+		this.termTemplate = _.template($("#term-template").html());
+	},
+
 	render: function() {
-		var template = _.template($("#term-template").html(),
-			{termText: "Testing text area; texttexttextetxtetxtetxtet",
-				acceptTermStr: "I accept the terms of ...",
-				acceptButtonStr: "Accept and continue",
-				backButtonStr: "Back to login"});
-		this.$el.html(template);
+		this.$el.html(this.layoutTemplate({
+			layoutStr: layoutStringsModel,
+			loggedIn: true
+		}));
+
+		$('#content-div').html(this.termTemplate({
+			termStr: termStringsModel
+		}));
 	}
 });
 
