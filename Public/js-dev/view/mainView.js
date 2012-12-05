47b1ddc4f8222db5fc44abb8c9f4b995c7e2e1b9
Luiza Silva <luiza@royalsflush.net>
2012-12-05 18:46:14 -0200

    Put strings file on main

--- Public/js-dev/view/mainView.js
+++ Public/js-dev/view/mainView.js
@@ -6,57 +6,42 @@ var MainView = Backbone.View.extend({
 	tabs: {'faltacursar': {
 			'li': 'main-faltacursar-li',
 			'div': 'main-faltacursar-div',
-			'str': '',
 			'href': '#faltacursar',
 			'view': faltacursarView
 		}, 
 		'microhorario': {
 			'li': 'main-microhorario-li',
 			'div': 'main-microhorario-div',
-			'str': '',
 			'href': '#microhorario',
 			'view': microhorarioView
 		},
 		'selected': {
 			'li': 'main-selected-li',
 			'div': 'main-selected-div',
-			'str': '',
 			'href': '#selected',
 			'view': selectedView
 		}},
 
 	defaultTab: 'faltacursar',		
-	
-	childrenViews: [
-		faltacursarView,
-		timetableView,
-		selectedView,
-		microhorarioView
-	],
 
 	//Cached variables
 	timetableDiv: '',
 	sidebarDiv: '',
-	timetableTable: '',
-	container: '',
 	tabsNav: '',
 
 	initialize: function() {
 		this.mainTemplate = _.template($('#main-template').html());
 		this.layoutTemplate = _.template($('#layout-template').html());
-		this.fetchStrings();
 		var me = this;
 	
 		$(window).resize(function() {
-		//	me.equalMainDivsHeight();
+			me.equalMainDivsHeight();
 		});
 	},
 
 	cache: function() {
 		this.timetableDiv = document.getElementById('main-timetable-div');
 		this.sidebarDiv = document.getElementById('main-sidebar-div');
-		this.timetableTable = document.getElementById('main-timetable-div table');
-		this.container = document.getElementById('container-div');
 		this.tabsNav = document.getElementById('main-tabs-nav');
 	},
 
@@ -77,13 +62,7 @@ var MainView = Backbone.View.extend({
 		$('#'+this.tabs[tab].li).addClass('active');
 		$('#'+this.tabs[tab].div).addClass('active');
 	
-	//	this.tabs[tab].view.resize();
-	},
-
-	fetchStrings: function() {
-		this.tabs.faltacursar.str='Falta Cursar';
-		this.tabs.microhorario.str='Micro Horario',
-		this.tabs.selected.str='Selecionadas';
+		this.tabs[tab].view.resize();
 	},
 
 	renderSubviews: function() {
@@ -105,7 +84,10 @@ var MainView = Backbone.View.extend({
 			layoutStr: layoutStringsModel,
 			loggedIn: true
 		}));
-		$('#content-div').html(this.mainTemplate({tabs: this.tabs}));
+		$('#content-div').html(this.mainTemplate({
+			tabs: this.tabs,
+			mainStr: mainStringsModel
+		}));
 		
 		this.renderSubviews();
 		this.rendered=true;
