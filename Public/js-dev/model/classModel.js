d424ef16b6d2fd9efdc13330ea217fbb3316cd47
Luiza Silva <luiza@royalsflush.net>
2013-01-17 01:11:23 -0200

    separated files for subject model and class model

---
+++ Public/js-dev/model/classModel.js
@@ -0,0 +1,127 @@
+var HorariosModel = Backbone.Model.extend({
+	get: function(attribute) {
+		return overriddenGet(this,attribute);
+	},
+
+	printHorario: function() {
+		var daysAbbr=classesTableStringsModel.get('daysAbbr');	
+		if (this.get('DiaSemana'))
+			return daysAbbr[this.get('DiaSemana')-2]+' '
+				+this.get('HoraInicial')+'-'+this.get('HoraFinal')+
+				' ('+this.get('Unidade')+');';
+		return classesTableStringsModel.get('SHF')+' ('+this.get('Unidade')+');';
+	}
+});
+
+var HorariosList = Backbone.Collection.extend({
+	model: HorariosModel
+});
+
+var BlockModel = Backbone.Model.extend({
+	get: function(attribute) {
+		return overriddenGet(this,attribute);
+	},
+
+	printBlock: function() {
+		return this.get('Destino')+' ('+this.get('Vagas')+' '+
+			classesTableStringsModel.get('placesStr')+');';
+	}
+});
+
+var BlocksList = Backbone.Collection.extend({
+	model: BlockModel
+});
+
+var ClassModel = Backbone.Model.extend({
+	initialize: function() {
+		var horariosArray = this.get('Horarios');
+		var horariosList = new HorariosList(horariosArray);
+		this.set(serverDictionary.get('Horarios'),horariosList);
+	
+		var blocksArray = this.get('Destinos');
+		var blocksList = new BlocksList(blocksArray);
+		this.set(serverDictionary.get('Destino')+'s',blocksList);
+	},
+
+	get: function(attribute) {
+		if (attribute=='Destinos') {
+			var chAttr = serverDictionary.get('Destino')+'s';
+			return Backbone.Model.prototype.get.call(this,chAttr);
+		}
+
+		var res=overriddenGet(this,attribute);
+
+		if (!res) res=overriddenGet(Backbone.Model.prototype.get.call(this, 'subject'),
+			attribute);
+		return res;
+	},
+
+	printSchedule: function() {
+		var div = document.createElement('div');
+		
+		_.each(this.get('Horarios').models, function(horario) {
+			var span=document.createElement('span');
+			span.innerHTML=horario.printHorario();
+
+			div.appendChild(span);
+			div.appendChild(document.createElement('br'));
+		});
+
+		return div.innerHTML;
+	},
+
+	printBlocks: function() {
+		var div = document.createElement('div');
+		
+		_.each(this.get('Destinos').models, function(block) {
+			var span=document.createElement('span');
+			span.innerHTML=block.printBlock();
+
+			div.appendChild(span);
+			div.appendChild(document.createElement('br'));
+		});
+
+		return div.innerHTML;
+	},
+
+	formatData: function() {
+		return {
+			'professorName': this.get('NomeProfessor'),
+			'code': this.get('CodigoTurma'),
+			'schedule': this.printSchedule(),
+			'block': this.printBlocks(),
+			'subjectCode': this.get('CodigoDisciplina'),
+			'classId': this.get('PK_Turma'),
+			'status': this.get('Situacao'),
+			'able': this.get('Apto')
+		};
+	}
+});
+
+ClassList = Backbone.Collection.extend({
+	model: ClassModel,
+
+	add: function(models, options) {
+		var me=this;
+		var array=new Array();
+
+		_.each(models, function(model) {
+			if (typeof model == me.model)
+				array.push(model);
+			else {
+				var idName = serverDictionary.get('PK_Turma');
+				var nModel;
+
+				if (options.subject)
+					nModel= new ClassModel($.extend({}, {id: model[idName], 
+						subject: options.subject}, model));
+				else
+					nModel= new ClassModel($.extend({}, {id: model[idName]}, model));
+					
+				array.push(nModel);
+			}
+		});
+
+		return Backbone.Collection.prototype.add.call(this,array,options);
+	}
+});
