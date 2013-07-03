c1a22ea7da45a98eb48af119799cbe6717ba59a3
Luiza Silva <luiza@royalsflush.net>
2013-07-02 21:22:30 -0300

    Changed makefiles a tad, removed object files

--- Public/js-dev/model/loggedInDialogsStringsModel.js
+++
@@ -1,9 +0,0 @@
-var SuggestionsStringsModel = Backbone.Model.extend({
-	defaults: {
-		'dialogTitle': 'Sugestões/Bugs/Comentários...',
-		'submitButtonLabel': 'Enviar comentário',
-		'resetButtonLabel': 'Limpar campo'
-	}
-});
-
-var suggestionsStringsModel = new SuggestionsStringsModel();
